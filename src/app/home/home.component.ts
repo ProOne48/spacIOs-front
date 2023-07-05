8fc9ef1dc161b240734f37016dca08de3904d699
SergioSanchez <sergio.sanchez@nucleoo.com>
2023-07-05 13:31:19 +0200

    Add more styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  user = AuthService.getSpaceOwnerData();
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+  }
+}
