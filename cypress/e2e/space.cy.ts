5160c33eb705b090a9be725b5a390b338dd92371
ProOne48 <sergiosaor@gmail.com>
2024-03-16 14:08:18 +0100

    Public-Space-#5 Fix Cypress test

--- cypress/e2e/space.cy.ts
+++ cypress/e2e/space.cy.ts
@@ -55,15 +55,18 @@ describe('Space test', () => {
 
   context('Routes redirections', () => {
     it('should redirect to the pdf view page and show the PDF', () => {
-      console.log(spaceId, spaceName);
       cy.intercept('GET', `${Cypress.env('API_URL')}/space/${spaceId}/pdf`, { fixture: `${spaceName}.pdf` }).as(
         'getPdf'
+      );
+      cy.intercept('GET', `${Cypress.env('API_URL')}/space/${spaceId}/reduced`, { fixture: 'space.json' }).as(
+        'getSpace'
       );
 
       cy.get('[data-cy=space-see-pdf]').click();
       cy.url().should('include', `/space/${spaceId}/pdf`);
       cy.wait('@getPdf');
-      cy.get('[data-cy=pdf-viewer]').should('be.visible');
+      cy.wait('@getSpace');
+      cy.get('[data-cy=pdf-viewer]').should('exist').should('be.visible');
     });
   });
 });
